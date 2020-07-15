b9dd84219467cace72068adeb2b887373f893239
mattb0101 <mattbaum666@googlemail.com>
2020-07-15 10:20:55 +0000

    Adding in pagination

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
-
-function getData(type, cb) {
+function getData(url, cb) {
   var XHR = new XMLHttpRequest();
 
-  XHR.open("GET", baseURL + type + "/");
+  XHR.open("GET", url);
   XHR.send();
 
   XHR.onreadystatechange = function () {
@@ -23,12 +21,30 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+
+function generatePaginationButtons(next, prev) {
+    if(next && prev) {
+        return `<button onclick = "writeToDocument('${prev}')">Previous</button>
+                <button onclick = "writeToDocument('${next}')">Next</button>`;
+    }   else if (next && !prev) {
+        return`<button onclick = "writeToDocument('${next}')">Next</button>`;
+    }   else if (!next && prev) {
+        return`<button onclick = "writeToDocument('${prev}')">Previous</button>`;
+    } 
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = ""
 
-    getData(type, function(data) {
+    getData(url, function(data) {
+
+        var pagination;
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
+
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -43,7 +59,7 @@ function writeToDocument(type) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
 
